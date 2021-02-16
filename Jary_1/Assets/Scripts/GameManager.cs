a38d0fbb01033bbc6ff2e73c7742737ef708b2bc
iamsherry000 <iamsherry000@gmail.com>
2021-02-16 17:50:46 +0800

    1up

---
+++ Jary_1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public InputField playerAnswerUI;
+    public int playerAnswer,num=-2;
+    public Text hints;
+    public Button Enter;
+    void Start()
+    {
+        UpdateHints("根據甜蜜指數 ");
+    }
+    
+    void UpdateHints(string message)
+    {
+        hints.text = message;
+    }
+
+    bool CanGetInputNum() 
+    {
+        return int.TryParse(playerAnswerUI.text, out playerAnswer);
+    }
+
+    public void Compare() 
+    {
+        //playerAnswer = int.Parse(playerAnswerUI.text);
+        if (!CanGetInputNum())
+        {
+            UpdateHints("numbers only !");
+            return;
+        }
+        num = playerAnswer;
+            if (num > -1 && num < 10)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load<Sprite>("pic1");
+                if (num == 0) UpdateHints("這麼糟糕麼...，我很抱歉，讓你這麼難過");
+            }
+            else if (num > 10 && num < 20)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load<Sprite>("pic3");
+            }
+            else if (num > 20 && num < 30)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load<Sprite>("pic4");
+            }
+            else if (num > 30 && num < 40)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load<Sprite>("pic5");
+            }
+            else if (num > 40 && num < 50)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load<Sprite>("pic6");
+            }
+            else if (num > 50 && num < 60)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load<Sprite>("pic7");
+            }
+            else if (num > 60 && num < 70)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load<Sprite>("pic8");
+            }
+            else if (num > 70 && num < 80)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load<Sprite>("pic2");
+            }
+            else if (num > 80 && num < 90)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load<Sprite>("pic10");
+            }
+            else if (num > 90 && num < 101)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load<Sprite>("pic11");
+            }
+            //else if (num == -2) gameObject.GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load<Sprite>("pic9");
+    }
+
+    public void ClearHint() 
+    {
+        UpdateHints("");
+    }
+
+    public void ReloadScene()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ LineRenderer/Assets/Scripts/testMesh.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testMesh : MonoBehaviour
+{
+    Vector3[] newVertices = new Vector3[100];
+    Vector2[] newUV=new Vector2[100];
+    int[] newTriangles=new int [3*100];
+    //public 
+    void Start()
+    {
+        for (int i = 0; i < 100; i++) newVertices[i] = new Vector3(i/10.0f-5.0f,0,0);
+        for (int i = 0; i < 100; i++) newUV[i] = new Vector2(0, 0);
+        for (int i = 0; i < 100; i++) 
+        {
+            newTriangles[i * 3 + 0] = 0;
+            newTriangles[i * 3 + 1] = 1;
+            newTriangles[i * 3 + 2] = i;
+        }
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        mesh.vertices = newVertices;
+        mesh.uv = newUV;
+        mesh.triangles = newTriangles;
+        //MeshFilter filter = GetComponent<MeshFilter>();
+        //MeshRenderer renderer = GetComponent<>
+    }
+    
+
+    void Update()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        //Vector3[] normals = mesh.normals;
+        Vector3 normal = new Vector3(0, 1, 0);
+        print(vertices.Length);
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            //vertices[i] += normals[i] * Mathf.Sin(Time.time);
+            vertices[i] += normal * Mathf.Sin(Time.time);
+        }
+
+        mesh.vertices = vertices;
+    }
+}
+
