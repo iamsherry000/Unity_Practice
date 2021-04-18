4f6bf2645461fad81ef4231d4e2b9a2723547346
iamsherry000 <iamsherry000@gmail.com>
2021-04-18 20:58:30 +0800

    2021/0410

---
+++ hair_pt1/Assets/Scripts/FrameRateCounter.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using TMPro;
+
+//best frame rate
+//the average
+//the worst 
+
+public class FrameRateCounter : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI display = default;
+    [SerializeField, Range(0.1f, 2f)]
+    float sampleDuration = 1f;
+    int frames;
+    float duration,bestDuration = float.MaxValue,worstDuration;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // to display the frame rate we need to know how much time has passed between the previous and current frame.//
+        // Time.deltaTime 受時間調整的時間刻度限制， 所以使用 Time.unscaledDeltaTime代替//
+        float frameDuration = Time.unscaledDeltaTime;
+        //display.SetText("FPS\n{0:0}\n000\n000",1f/frameDuration);
+        frames += 1;
+        duration += frameDuration;
+        //display.SetText("FPS\n{0:0}\n000\n000", frames/duration);
+        //Average frame rate over one second.//
+        if (frameDuration < bestDuration) {
+            bestDuration = frameDuration;
+        }
+        if (frameDuration > worstDuration) {
+            worstDuration = frameDuration;
+        }
+        
+        if (duration >= sampleDuration) {
+            display.SetText("FPS\n{0:0}\n{1:0}\n{2:0}",1f/bestDuration,frames/duration,1f/worstDuration);
+            frames = 0;
+            duration = 0f;
+        }
+        bestDuration = float.MaxValue;
+        worstDuration = 0f;
+    }
+}
