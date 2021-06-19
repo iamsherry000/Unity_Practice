dbd8c743b7e456ac4e6a3456cb070c5f0226bf02
iamsherry000 <iamsherry000@gmail.com>
2021-06-19 22:14:15 +0800

    20210619_2214

---
+++ PosTry/Assets/Scripts/forPosTry4/HandPosition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+using Valve.VR;
+
+public class HandPosition : MonoBehaviour
+{
+    public Rigidbody attachPoint;//rigidbody
+    private SteamVR_Input_Sources inputSource; //get inputSource for devices
+    public SteamVR_Action_Boolean TriggerClick;//板機鍵按鈕
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
