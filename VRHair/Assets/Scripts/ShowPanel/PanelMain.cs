06f73eefba67603da8f1191a50321555745ee160
iamsherry000 <iamsherry000@gmail.com>
2021-06-22 18:30:11 +0800

    VRHairCreater1.1

---
+++ VRHair/Assets/Scripts/ShowPanel/PanelMain.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelMain : MonoBehaviour
+{
+    public void PaintClick() {
+        GameObject.Find("RightHand").GetComponent<drawer>().enabled = true;
+        UIManager.Instance.ShowPanel("RPanel_Paint");
+    }
+}
