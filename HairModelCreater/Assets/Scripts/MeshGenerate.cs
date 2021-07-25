ad767c6ec54b5e9a4cd1adf6e2abb6bd5aea6584
iamsherry000 <iamsherry000@gmail.com>
2021-07-25 12:16:08 +0800

    Sherry0725_1216

---
+++ HairModelCreater/Assets/Scripts/MeshGenerate.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class MeshGenerate : MonoBehaviour
+{
+    private Mesh mesh;
+    Material GetHairColor;
+    MeshCollider HairCollider;
+
+    int[] triangle;
+
+    Vector2[] uvs;
+    Vector3[] vertices;
+    Vector4[] tangents;
+
+    public void GenerateMesh(List<Vector3>GetPointPos, int GetHairWidth)
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh(); // meshfilter中的mesh指給程式宣告的mesh
+        GetHairColor = GetComponent<Renderer>().material;
+        GetHairColor.color = Color.red;
+        GetComponent<MeshRenderer>().material = GetHairColor;
+        mesh.name = "HairMesh";
+        if (gameObject.GetComponent<MeshCollider>() == null) HairCollider = gameObject.AddComponent<MeshCollider>();
+        else HairCollider = gameObject.GetComponent<MeshCollider>();
+        HairCollider.sharedMesh = mesh;
+
+        //設定index大小符合 GetPointPos 的Count
+        uvs = new Vector2[GetPointPos.Count];
+        vertices = new Vector3[GetPointPos.Count];
+        tangents = new Vector4[GetPointPos.Count];
+        //丟值
+        for(int i = 0; i < GetPointPos.Count; i++)
+        {
+            vertices[i] = GetPointPos[i];
+            uvs[i].x = GetPointPos[i].x;
+            uvs[i].y = GetPointPos[i].y;
+            tangents[i] = new Vector4(1f, 0f, 0f, -1f);
+        }
+        //指給mesh使用
+        
+        mesh.vertices = vertices;
+        mesh.uv = uvs;
+        mesh.tangents = tangents;
+
+        int point = ((GetPointPos.Count / (3 + (GetHairWidth - 1) * 2) - 1)) * 2 * GetHairWidth;
+        triangle = new int[point * 6];
+        int t = 0; // triangle 的 index，負責指標位置值
+
+        for (int vi = 0, k = 0, x = 1; x <= point; x++, vi += k)
+        {
+            t = SetQuad(triangle, t, vi, vi + 1, vi + 3 + (2 * (GetHairWidth - 1)), vi + 4 + (2 * (GetHairWidth - 1)));
+            if (x % (GetHairWidth * 2) != point % (GetHairWidth * 2)) k = 1;  //在同一行
+            else k = 2;  //對vi的累加  (需換行時)
+        }
+
+        mesh.triangles = triangle;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+
+    }
+
+    private static int SetQuad(int[] triangles, int i, int v0, int v1, int v2, int v3)
+    {
+        triangles[i] = v0;
+        triangles[i + 1] = v1;
+        triangles[i + 2] = v2;
+        triangles[i + 3] = v2;
+        triangles[i + 4] = v1;
+        triangles[i + 5] = v3;
+        return i + 6;
+    }
+}
---
+++ HairModelCreater/Assets/Scripts/functions/test.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Point
+{
+    public List<Vector3> list; 
+}
+[System.Serializable]
+public class PointList
+{
+    public List<Point> list;
+}
+
+public class test : MonoBehaviour
+{
+    //< <A:a1-a3> <B:b1-b3> >  
+    // <list<list<vector3>>>
+    public Point A = new Point();
+    public Point B = new Point();
+    public Point C = new Point();
+
+    public List<Vector3> D = new List<Vector3>();
+    public PointList ListOfPointLists = new PointList();
+
+    private void Start()
+    {
+        Vector3 a1 = new Vector3(1, 1, 1);
+        Vector3 a2 = new Vector3(2, 1, 1);
+        Vector3 a3 = new Vector3(3, 1, 1);
+
+        Vector3 b1 = new Vector3(1, 1, 1);
+        Vector3 b2 = new Vector3(1, 2, 1);
+        Vector3 b3 = new Vector3(1, 3, 1);
+
+        Vector3 d0 = new Vector3(0, 1, 1);
+        D.Add(d0);
+
+        ListOfPointLists.list[0].list.Add(a1);
+        ListOfPointLists.list[0].list.Add(a2);
+        ListOfPointLists.list[0].list.Add(a3);
+    }
+
+
+}
