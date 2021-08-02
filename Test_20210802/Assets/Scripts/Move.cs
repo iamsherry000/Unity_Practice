262d4f78c1320a83704f68cfd78f0e27cd37850e
iamsherry000 <iamsherry000@gmail.com>
2021-08-02 21:26:52 +0800

    Sherry0802_2126

--- Test_20210802/Assets/Scripts/Move.cs
+++ Test_20210802/Assets/Scripts/Move.cs
@@ -8,6 +8,7 @@ public class Move : MonoBehaviour
     Vector3 mousePos;
     Vector3 distance;
     //public Vector3 monsterPos, originPos;
+
     void Start()
     {
         //monsterPos = monster.transform.position; //green cube
@@ -18,21 +19,27 @@ public class Move : MonoBehaviour
     {
         distance.x = monster.transform.position.x > origin.transform.position.x ? monster.transform.position.x - origin.transform.position.x : origin.transform.position.x - monster.transform.position.x;
         distance.y = monster.transform.position.y > origin.transform.position.y ? monster.transform.position.y - origin.transform.position.y : origin.transform.position.y - monster.transform.position.y;
-        
+        //distance.x = monsterPos.x > originPos.x ? monsterPos.x - originPos.x : originPos.x - monsterPos.x;
+        //distance.y = monsterPos.y > originPos.y ? monsterPos.y - originPos.y : originPos.y - monsterPos.y;
     }
     void Update()
     {
         mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
+        
         countdist();
         if (Input.GetMouseButtonDown(0)) //滑鼠按下 //到時候不用(因為怪物自己會走)
-        {            
+        {
+            //monsterPos = monster.transform.position; //green cube
+            
             if (distance.x > 3f || distance.y > 3f)
             {
                 monster.transform.position = origin.transform.position;
+                //monsterPos = originPos; //green cube
             }
             else
             {
-                monster.transform.position = mousePos; 
+                monster.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f)); ; 
+                //monsterPos = mousePos;
             }
         }    
     }
